222bcc34343d2ccb69040d9744fe293f0e4eba0c
YashKandalkar <yashsk.yk@gmail.com>
2021-09-18 14:03:40 +0530

    fix: fixed bugs in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import {useRef} from "react";
 import NavBar from "./components/Navbar/Navbar";
 import {
   MapContainer,
@@ -9,60 +8,69 @@ import {
 } from "react-leaflet";
 import "./css/App.css";
 import useGeoLocation from "./hooks/useGeoLocation";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
 
-import Button from '@mui/material/Button';
-
+import Button from "@mui/material/Button";
 
 const App = ({ zoom = 13, scrollWheelZoom = true }) => {
   const location = useGeoLocation();
-  const mapRef = useRef();
   const ZOOM_LEVEL = 9;
-  const showMyLocation = () => {
-    
-    if ( location.loaded && !location.error) {
-      mapRef.current.leafletElement.flyTo(
-    
-        [location.coordinates.lat, location.coordinates.lng],
-        ZOOM_LEVEL,
-        { animate: true }
-      );
-    } else {
-      alert(location.error.message);
-    }
-  };
-  
+
+  if (location.loaded && location.error) {
+    // TODO: use GeoIp API to get approximate location
+  }
+
+  console.log(location);
+
   return (
     <>
-    <NavBar />
-    
-    <MapContainer
-      
-      style={{
-        height: "100vh",
-        position: "fixed",
-        top: "12vh",
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }}
-      center={[location.coordinates.lat, location.coordinates.lng]}
-      zoom={zoom}
-      ref={mapRef} 
-      scrollWheelZoom={scrollWheelZoom}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      
-      {location.loaded && !location.error && (
-        <Marker position={[location.coordinates.lat,location.coordinates.lng]}></Marker>
-      )}
-      {/* <LocationMarker /> */}
-    </MapContainer>
-    <div>
-        <Button color="inherit" onClick={showMyLocation}>Locate me</Button>
+      <NavBar />
+
+      {location.loaded ? (
+        !location.error ? (
+          <MapContainer
+            style={{
+              height: "100vh",
+              position: "fixed",
+              top: "10vh",
+              right: 0,
+              left: 0,
+              bottom: 0,
+            }}
+            center={[location.coordinates.lat, location.coordinates.lng]}
+            zoom={zoom}
+            scrollWheelZoom={scrollWheelZoom}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            {location.loaded && !location.error && (
+              <Marker
+                position={[location.coordinates.lat, location.coordinates.lng]}
+              ></Marker>
+            )}
+            {/* <LocationMarker /> */}
+          </MapContainer>
+        ) : (
+          <div>Location Access not given, use GeoIP location here.</div>
+        )
+      ) : (
+        <div
+          style={{
+            minHeight: "90vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h5" component="h2">
+            Please enable location access in your browser.
+          </Typography>
         </div>
+      )}
     </>
   );
 };
