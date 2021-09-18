2930af393ee88515e7b43cd533994a198c7ed968
Rishi <chiragt1805@gmail.com>
2021-09-18 23:24:50 +0530

    firbase auth

--- src/firebase/authApi.js
+++ src/firebase/authApi.js
@@ -3,11 +3,12 @@ import {
   signInWithPopup,
   getAuth,
   signOut as firebaseSignOut,
-} from "firebase/auth";
-
+  
+}from "firebase/auth";
+import {firebaseApp} from "./init"
 export const signInWithGoogle = (onSuccess, onError) => {
   const provider = new GoogleAuthProvider();
-  const auth = getAuth();
+  const auth = getAuth(firebaseApp);
   // auth().useDeviceLanguage();
   signInWithPopup(auth, provider)
     .then((result) => {
